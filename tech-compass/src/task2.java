d144f665f56094a20db6ed777849f703ea81d1e8
YuTaka2018 <voca39chan@gmail.com>
2020-02-21 19:52:46 +0900

    コミットメッセージ

---
+++ tech-compass/src/task2.java
@@ -0,0 +1,12 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		//正しい回答
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+
+		//System.out.println("hogehoge")
+	}
+	//System.out.println("hogehoge2")
+}
