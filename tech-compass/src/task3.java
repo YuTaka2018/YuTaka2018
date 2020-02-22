9e55e29c85456f068e3b9adfea6a5fb0973d7a3a
YuTaka2018 <voca39chan@gmail.com>
2020-02-22 10:45:09 +0900

    コミットメッセージ

---
+++ tech-compass/src/task3.java
@@ -0,0 +1,18 @@
+
+public class task3 {
+	public static void main(String[] args) {
+
+	int intNum = 219;
+	double doubleNum = 364.2;
+	long longNum = 123456L;
+
+	long newIntNum = (long) intNum;
+	float newDoubleNum = (float) doubleNum;
+	double newLongNum = (double) longNum;
+
+	System.out.println(newIntNum);
+	System.out.println(newDoubleNum);
+	System.out.println(newLongNum);
+
+	}
+}
---
+++ tech-compass/変数・定数/task1.java
@@ -0,0 +1,12 @@
+public class task1 {
+
+	public static void main(String[] args) {
+		float syosu = 31.2;
+		String moji = "Java研修";
+		int seisu = 9;
+		double seisu2 = 2147483648;
+		char kanji = '才';
+
+	}
+
+}
